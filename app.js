715e98b5a64c7e0763811ffbefea9eb25b47923a
Nguyenphuc1610 <nope1610@gmail.com>
2024-10-30 00:03:33 +0700

    update v2

--- app.js
+++ app.js
@@ -1,46 +1,39 @@
-// src/app.js
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db.config');
-const authRoutes = require('./routes/auth.routes');
-const userRoutes = require('./routes/user.routes');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-// Load environment variables
-dotenv.config();
+// ROUTER
+const authRouter = require("./src/routes/authRoutes");
+const userRouter = require("./src/routes/userRoutes");
 
-// Create Express app
 const app = express();
 
-// Connect to MongoDB
-connectDB();
+// MIDDLEWARES
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
-// Middleware
+const corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-// Routes
-app.get('/info', (req, res) => {
-  res.json({
-    data: {
-      fullName: "Nguyen TRAN HUU PHUC",
-      studentCode: "QE170051"
-    }
-  });
+// ROUTES
+app.use("/info", (_, res, next) => {
+    res.status(200).json({
+        data: {
+            fullName: "Nguyen Van A",
+            studentCode: "QNUO1234",
+        },
+    });
+    next();
 });
+app.use("/auth", authRouter);
+app.use("/users", userRouter);
 
-app.use('/auth', authRoutes);
-app.use('/users', userRoutes);
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    success: false,
-    message: 'Internal server error'
-  });
-});
-
-// Start server
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
